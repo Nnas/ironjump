f8e928ae6cae85611b49d1cd178aca403807fe2a
Nnas <batal.anas@gmail.com>
2018-04-11 21:36:27 +0200

    modification constans

--- js/constants.js
+++ js/constants.js
@@ -2,4 +2,4 @@ var width = 1000; // Default value is 1000
 var height = 1700; // Default value is 1700
 gravity = 5; // Default value is 5
 intervalTimeout = 25; // Default value is 25
-var debug = true; // Change it to false to remove the grid
+var debug = false; // Change it to false to remove the grid
